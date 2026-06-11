506e8702f8091d808502c8ee4504f7c398facbcf
aquadolphinw <redpandas2651@gmail.com>
2026-06-11 16:00:10 -0500

    Ordered Ranking

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if filepath:
     # inputs (base year, number of years forward)
     baseyear = int(input("Enter the base year (2024 for SY24-25): "))
     numyears = int(input("Enter the number of years forward: "))
-    loomistoggle = input("Loomis K2, Longwood 3-8? (y/n) Otherwise Loomis K-3, Longwood 4-8: ").lower() == 'y'
+    loomistoggle = input("Loomis K-2, Longwood 3-8? (y/n) Otherwise Loomis K-3, Longwood 4-8: ").lower() == 'y'
 
     config = ParameterConfig(baseyear, numyears, loomistoggle)
 
