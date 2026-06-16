689d2b6ac2ec7dae2e147f2fe45f28c4aedd1992
Chloe <chloelei0706@gmail.com>
2026-06-16 12:56:50 -0500

    updated numyears

--- main.py
+++ main.py
@@ -29,23 +29,19 @@ if filepath:
 
     # inputs (base year, number of years forward)
     baseyear = int(input("Enter the base year (2024 for SY24-25): "))
+    numyears = int(input("Enter the number of years forward: "))
     loomistoggle = input("Loomis K-2, Longwood 3-8? (y/n) Otherwise Loomis K-3, Longwood 4-8: ").lower() == 'y'
 
-    config = ParameterConfig(baseyear, loomistoggle)
-    config.get_target_years(df)
+    config = ParameterConfig(baseyear, numyears, loomistoggle)
 
     calculator = RetentionCalculator(df, config)
     retention_rates = calculator.run()
 
     print(retention_rates)
-<<<<<<< HEAD
     
     # graph trends for 1-year retention
     graph = calculator.graph()
     plt.show()
-=======
-
->>>>>>> 9283868b24a3906d90e178a214b87f2749d843bb
 
 else:
     print("No file was selected.")
