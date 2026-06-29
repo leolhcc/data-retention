56fed71e51a3428f62b9dc9e9e10fa3136820310
Chloe <chloelei0706@gmail.com>
2026-06-29 12:02:22 -0500

    account for empty grade filter input

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ if filepath:
     baseyear = int(input("Enter the base year (2025 for SY24-25): "))
     numyears = int(input("Enter the number of years forward: "))
     loomistoggle = input("Loomis K-2, Longwood 3-8? (y/n) Otherwise Loomis K-3, Longwood 4-8: ").lower() == 'y'
-    gradefilter = int(input("Enter the grade level (e.g., 0, 1, 2, 3, etc.): "))
+    gradeinput = input("Enter the grade level (e.g., 0, 1, 2, 3, etc.): ")
+    gradefilter = int(gradeinput) if gradeinput.strip() else None
     config = ParameterConfig(baseyear, numyears, loomistoggle, gradefilter)
 
     retention_calculator = RetentionCalculator(df, config)
